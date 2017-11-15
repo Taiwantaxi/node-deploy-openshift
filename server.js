c999a1f0c9c45015e12de4f8a02744bace099300
Bo-Rong <brown.huang@taiwantaxi.com.tw>
2017-11-15 16:31:46 +0800

    commit

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var app = require('express')();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+const IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.get('/', function(req, res){
   console.log(req.body);
   res.send('Hello World');
 });
 
-app.listen(PORT, function(){
+app.listen(PORT, IP, function(){
   console.log(`App listening on port ${PORT}!`)
 });
 
